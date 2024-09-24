97f3e7192c05a4d188fdb0a6716350f291f8ccb9
Erfan Shadkam <erfanshadkam@outlook.com>
2024-09-25 02:21:15 +0300

    changes4

--- server.py
+++ server.py
@@ -14,8 +14,23 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+# def write_to_file(data):
+#     with open('database.txt',mode='a') as database:
+#         email = data['email']
+#         name = data['name']
+#         comments = data['comments']
+#         file = database.write(f'\n{email}, {name}, {comments}')
+
+# def write_to_csv(data):
+#     with open('database.csv',mode='a') as database2:
+#         email = data['email']
+#         name = data['name']
+#         comments = data['comments']
+#         csv_writer = csv.writer(database2,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL,)
+#         csv_writer.writerow([email,name,comments])
+
 def email_sender(data) :
-    html = Template(Path('./static/sender.html').read_text())
+    # html = Template(Path('sender.html').read_text())
     email = EmailMessage()
     email['From'] = 'server'
     email['To'] = 'erfanshadkam@outlook.com'
@@ -23,7 +38,7 @@ def email_sender(data) :
     email1 = data['email']
     name = data['name']
     comments1 = data['comments']
-    email.set_content(html.substitute({'name': email1 , 'email': name, 'comment': comments1}), 'html')
+    email.set_content(f'Name : {name}\n Email: {email1}\n Comments:{comments1}')
 
     with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
         smtp.ehlo()

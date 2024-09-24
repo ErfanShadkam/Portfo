e13afaf4150136e02ad53de0a20a7a8449eb2510
Erfan Shadkam <erfanshadkam@outlook.com>
2024-09-24 05:19:22 +0300

    email-sender-compelet

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-
 from flask import Flask , render_template,request,redirect,url_for
 import csv
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 app = Flask(__name__)
-
 
 @app.route("/")
 def my_home():
@@ -19,13 +21,31 @@ def html_page(page_name):
 #         comments = data['comments']
 #         file = database.write(f'\n{email}, {name}, {comments}')
 
-def write_to_csv(data):
-    with open('database.csv',mode='a') as database2:
-        email = data['email']
-        name = data['name']
-        comments = data['comments']
-        csv_writer = csv.writer(database2,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL,)
-        csv_writer.writerow([email,name,comments])
+# def write_to_csv(data):
+#     with open('database.csv',mode='a') as database2:
+#         email = data['email']
+#         name = data['name']
+#         comments = data['comments']
+#         csv_writer = csv.writer(database2,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL,)
+#         csv_writer.writerow([email,name,comments])
+
+def email_sender(data) :
+    html = Template(Path('index.html').read_text())
+    email = EmailMessage()
+    email['From'] = 'server'
+    email['To'] = 'erfanshadkam@outlook.com'
+    email['Subject'] = 'you have contract'
+    email1 = data['email']
+    name = data['name']
+    comments1 = data['comments']
+    email.set_content(html.substitute({'name': email1 , 'email': name, 'comment': comments1}), 'html')
+
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('emailsenderformywebsite@gmail.com', 'vgnf kefx dkkw yjwv')
+        smtp.send_message(email)
+
 
 
 
@@ -34,13 +54,13 @@ def write_to_csv(data):
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_csv(data)
+        email_sender(data)
         return  'thank you i will get in touch with you'
     else:
         return 'something went wrong'
 
 
-#sdas
+
 
 
 app.run(host='127.0.0.1',port=8000,debug=True)
